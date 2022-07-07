28298166d20729bc98e082187e927be3ee1aac1a
Chrissie Ugay <chrissie.anne.k.ugay@accenture.com>
2022-07-07 20:38:34 +0800

    challenge 2 completion

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastSpawn;
+    private float currentSpawn;
+    private float spawnInterval = 0.5f;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float spawnDifference = lastSpawn - currentSpawn;
+        lastSpawn = Time.time;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spawnDifference > spawnInterval)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            currentSpawn = Time.time;
+        }
+    }
+}
--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     public float topBound = 30;
-    public float lowerBound = -10;
+    private float lowerBound = -10;
 
     // Start is called before the first frame update
     void Start()
